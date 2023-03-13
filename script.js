eff11bdd6061bc814a33d39803fc63fd7f829462
Strjela <nikola.jelic44@gmail.com>
2023-03-13 18:01:07 +0100

    working

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const polje = document.querySelectorAll(".field");
+const infoDis = document.querySelector("#info");
+
+console.log(infoDis);
 
 let currentPlayer = "x";
 
@@ -16,23 +19,51 @@ const winningAxes = [
 polje.forEach((el) => {
   el.addEventListener("click", () => {
     setSimbol(el);
+    displayImage(el);
     checkWinner(count());
+    checkDraw(count());
     switchPlayer(currentPlayer);
+    el.disabled = true;
+    console.log(currentPlayer);
   });
 });
 
+function a(b) {
+  if ((infoDis.textContent = `o won!` || infoDis.textContent == `x won!`)) {
+    polje.disabled = true;
+    console.log("aa");
+  } else return;
+}
+
 function setSimbol(x) {
-  if (x.textContent != "") {
-    return;
-  } else return (x.textContent = currentPlayer);
+  return (x.textContent = currentPlayer);
 }
 
 function switchPlayer(player) {
   if (player === "x") {
-    return (currentPlayer = "o");
+    currentPlayer = "o";
   } else {
-    return (currentPlayer = "x");
+    currentPlayer = "x";
   }
+}
+
+let xPlyImg = "./pictures/xplayer.png";
+let oPlyImg = "./pictures/oplayer.png";
+
+const din = document.querySelector(".dinamo");
+
+function displayImage(a) {
+  let imgElement = document.createElement("img");
+  imgElement.classList.add("dinamo");
+  if (currentPlayer === "x") {
+    imgElement.src = xPlyImg;
+    infoDis.textContent = "Player X's turn!";
+  } else if (currentPlayer === "o") {
+    imgElement.src = oPlyImg;
+    infoDis.textContent = "Player O's turn!";
+  }
+
+  a.append(imgElement);
 }
 
 let checkSubset = (parentArray, subsetArray) => {
@@ -51,10 +82,38 @@ function count() {
   return arr;
 }
 
+function checkDraw(item) {
+  if (item.length >= 5) {
+    infoDis.textContent = "Draw!";
+  }
+}
+
+let dis = false;
+
 function checkWinner(arr) {
   winningAxes.forEach((combination) => {
     if (checkSubset(arr, combination)) {
-      return console.log("bbbbbbbbb");
+      infoDis.textContent = `Player ${currentPlayer.toLocaleUpperCase()} won!`;
+      polje.forEach((el) => {
+        el.disabled = true;
+      });
     }
   });
 }
+
+const restBtn = document.querySelector("#rstBtn");
+
+restBtn.addEventListener("click", retartEvery);
+
+function retartEvery() {
+  polje.forEach((item) => {
+    item.textContent = "";
+    while (item.hasChildNodes()) {
+      item.removeChild(item.firstChild);
+    }
+    item.disabled = false;
+  });
+  infoDis.textContent = "Player X's turn!";
+  currentPlayer = "x";
+}
+
