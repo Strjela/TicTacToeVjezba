8bfc0af9818dcb70f62af007fd89a1dd985c68d8
Strjela <nikola.jelic44@gmail.com>
2023-03-13 18:37:29 +0100

    finished with one player

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const polje = document.querySelectorAll(".field");
 const infoDis = document.querySelector("#info");
 
-console.log(infoDis);
 
 let currentPlayer = "x";
 
@@ -16,62 +15,56 @@ const winningAxes = [
   [2, 4, 6],
 ];
 
+//main
 polje.forEach((el) => {
   el.addEventListener("click", () => {
-    setSimbol(el);
+    // setSimbol(el);
     displayImage(el);
-    checkWinner(count());
     checkDraw(count());
+    checkWinner(count());
+
     switchPlayer(currentPlayer);
     el.disabled = true;
     console.log(currentPlayer);
   });
 });
 
-function a(b) {
-  if ((infoDis.textContent = `o won!` || infoDis.textContent == `x won!`)) {
-    polje.disabled = true;
-    console.log("aa");
-  } else return;
-}
-
+//fukcija stavlja u dom element trenutnog igraca, text-size: 0 te se x ili o ne vidi
 function setSimbol(x) {
   return (x.textContent = currentPlayer);
 }
 
+//mjenja igraca
 function switchPlayer(player) {
   if (player === "x") {
-    currentPlayer = "o";
+    return (currentPlayer = "o");
   } else {
-    currentPlayer = "x";
+    return (currentPlayer = "x");
   }
 }
 
+//dodaje sliku znaka na stranicu ovisno o currentPlayeru
 let xPlyImg = "./pictures/xplayer.png";
 let oPlyImg = "./pictures/oplayer.png";
-
-const din = document.querySelector(".dinamo");
+const din = document.querySelector(".simbol");
 
 function displayImage(a) {
   let imgElement = document.createElement("img");
-  imgElement.classList.add("dinamo");
+  imgElement.classList.add("simbol");
   if (currentPlayer === "x") {
     imgElement.src = xPlyImg;
-    infoDis.textContent = "Player X's turn!";
+    infoDis.textContent = "Player O's turn!";
+    a.textContent = currentPlayer;
   } else if (currentPlayer === "o") {
     imgElement.src = oPlyImg;
-    infoDis.textContent = "Player O's turn!";
+    infoDis.textContent = "Player X's turn!";
+    a.textContent = currentPlayer;
   }
-
   a.append(imgElement);
 }
 
-let checkSubset = (parentArray, subsetArray) => {
-  return subsetArray.every((el) => {
-    return parentArray.includes(el);
-  });
-};
-
+//fukcnija koja vraca array tako sta provjeri koji je text na polju
+//koje je kliknuto te ovisno o currentPlayeru pusa index tog polja u taj array
 function count() {
   let arr = [];
   polje.forEach((elem, num) => {
@@ -82,14 +75,17 @@ function count() {
   return arr;
 }
 
-function checkDraw(item) {
-  if (item.length >= 5) {
-    infoDis.textContent = "Draw!";
-  }
-}
+// uzima dva arraya i uspodeduje dali od neki od arraya isti
+//uzima winningAxes i arr iz funkcije count
+let checkSubset = (parentArray, subsetArray) => {
+  return subsetArray.every((el) => {
+    return parentArray.includes(el);
+  });
+};
 
-let dis = false;
-
+//funkcija prima arr iz count()
+//i tu arr i winningAxes provodi kroz  checkSubset() te ako
+//je true displaya winnera i iskljuci click opciju na button od fielda(zaustavlja igru)
 function checkWinner(arr) {
   winningAxes.forEach((combination) => {
     if (checkSubset(arr, combination)) {
@@ -101,8 +97,20 @@ function checkWinner(arr) {
   });
 }
 
+//provjerava dali je draw tako sto uzima arr iz count() funkcije
+//a draw ce biti ako je duljina arraya 5 jer je to max broj poteza
+function checkDraw(item) {
+  if (item.length >= 5) {
+    infoDis.textContent = "Draw!";
+  }
+}
+
+
+
 const restBtn = document.querySelector("#rstBtn");
 
+//na ovaj nacin sam triba napravit main al nema veze
+//ugl. restart button se moze stisnit bilo kad i vraca sve na pocetno stanje
 restBtn.addEventListener("click", retartEvery);
 
 function retartEvery() {
